dd356792ac67f3d238b4c3ccfc54db8415f7be4f
Guo-zy <674402145@qq.com>
2020-07-24 07:59:26 +0800

    [Guess Number] [Yancy] refactor : remove all the isXXX function , update play function  and add the isInputNumberSValid function

--- src/main/java/GuessNumberDemo/GuessNumber.java
+++ src/main/java/GuessNumberDemo/GuessNumber.java
@@ -3,59 +3,68 @@ package GuessNumberDemo;
 public class GuessNumber {
 
     public String play(int[] answer, int[] inputNumbers) {
-        if (is4A0B(answer, inputNumbers)) return "4A0B";
-        if (is0A0B(answer, inputNumbers)) return "0A0B";
-        if (isXA0B(answer, inputNumbers)) {
-            int A = findA(answer , inputNumbers);
-            return String.valueOf(A) + "A0B";
+//        if (is4A0B(answer, inputNumbers)) return "4A0B";
+//        if (is0A0B(answer, inputNumbers)) return "0A0B";
+//        if (isXA0B(answer, inputNumbers)) {
+//            int A = findA(answer , inputNumbers);
+//            return String.valueOf(A) + "A0B";
+//        }
+//        if(is0AXB(answer,inputNumbers)){
+//            int B = findB(answer, inputNumbers);
+//            return "0A" + String.valueOf(B) + "B";
+//        }
+//        if(isXAXB(answer,inputNumbers)){
+//            int A = findA(answer, inputNumbers);
+//            int B = findB(answer,inputNumbers);
+//            return String.valueOf(A) + "A" + String.valueOf(B) + "B";
+//        }
+        if (isInputNumbersValid(inputNumbers)) {
+            String A = String.valueOf(findA(answer, inputNumbers));
+            String B = String.valueOf(findB(answer, inputNumbers));
+            return A + "A" + B + "B";
         }
-        if(is0AXB(answer,inputNumbers)){
-            int B = findB(answer, inputNumbers);
-            return "0A" + String.valueOf(B) + "B";
-        }
-        if(isXAXB(answer,inputNumbers)){
-            int A = findA(answer, inputNumbers);
-            int B = findB(answer,inputNumbers);
-            return String.valueOf(A) + "A" + String.valueOf(B) + "B";
-        }
-        return "0A4B";
+        return null;
     }
 
-    private boolean isXAXB(int[] answer, int[] inputNumbers) {
-        int A = findA(answer, inputNumbers);
-        int B = findB(answer, inputNumbers);
-        if(A != 0 && B != 0)
-            return true;
-        return false;
+    private boolean isInputNumbersValid(int[] inputNumbers) {
+        return true;
     }
 
-    private boolean is0AXB(int[] answer, int[] inputNumbers) {
-        int A = findA(answer, inputNumbers);
-        int B = findB(answer, inputNumbers);
-        if(A == 0 && B != 0)
-            return true;
-        return false;
-    }
-
-    private boolean isXA0B(int[] answer, int[] inputNumbers) {
-        int[] flag = new int[10];
-        int A = findA(answer, inputNumbers);
-        int B = findB(answer, inputNumbers);
-        if(A != 0 && B == 0) return true;
-        return false;
-    }
+//    private boolean isXAXB(int[] answer, int[] inputNumbers) {
+//        int A = findA(answer, inputNumbers);
+//        int B = findB(answer, inputNumbers);
+//        if (A != 0 && B != 0)
+//            return true;
+//        return false;
+//    }
+//
+//    private boolean is0AXB(int[] answer, int[] inputNumbers) {
+//        int A = findA(answer, inputNumbers);
+//        int B = findB(answer, inputNumbers);
+//        if (A == 0 && B != 0)
+//            return true;
+//        return false;
+//    }
+//
+//    private boolean isXA0B(int[] answer, int[] inputNumbers) {
+//        int[] flag = new int[10];
+//        int A = findA(answer, inputNumbers);
+//        int B = findB(answer, inputNumbers);
+//        if (A != 0 && B == 0) return true;
+//        return false;
+//    }
 
     private int findB(int[] answer, int[] inputNumbers) {
         int B = 0;
-        int [] base = new int[10];
-        for(int i = 0; i < answer.length; i ++){
-                if(answer[i] != inputNumbers[i]){
-                    base[answer[i]] ++;
-                    base[inputNumbers[i]] ++;
-                }
+        int[] base = new int[10];
+        for (int i = 0; i < answer.length; i++) {
+            if (answer[i] != inputNumbers[i]) {
+                base[answer[i]]++;
+                base[inputNumbers[i]]++;
+            }
         }
-        for(int num : base){
-            if(num == 2) B ++;
+        for (int num : base) {
+            if (num == 2) B++;
         }
         return B;
     }
@@ -69,20 +78,20 @@ public class GuessNumber {
     }
 
 
-    private boolean is0A0B(int[] answer, int[] inputNumbers) {
-        int A = findA(answer,inputNumbers);
-        int B = findB(answer, inputNumbers);
-        if(A == B && A == 0)
-            return true;
-        return false;
-    }
-
-    private boolean is4A0B(int[] answer, int[] inputNumbers) {
-        for (int i = 0; i < answer.length; i++) {
-            if (answer[i] != inputNumbers[i]) return false;
-        }
-        return true;
-    }
+//    private boolean is0A0B(int[] answer, int[] inputNumbers) {
+//        int A = findA(answer, inputNumbers);
+//        int B = findB(answer, inputNumbers);
+//        if (A == B && A == 0)
+//            return true;
+//        return false;
+//    }
+//
+//    private boolean is4A0B(int[] answer, int[] inputNumbers) {
+//        for (int i = 0; i < answer.length; i++) {
+//            if (answer[i] != inputNumbers[i]) return false;
+//        }
+//        return true;
+//    }
 
 
 }
