64fa62623f726afd791578f020430a9a36b80058
Guo-zy <674402145@qq.com>
2020-07-24 00:46:06 +0800

    [GuessNumber] [Yancy] refactor : add is0A0B function

--- src/main/java/GuessNumberDemo/GuessNumber.java
+++ src/main/java/GuessNumberDemo/GuessNumber.java
@@ -4,16 +4,16 @@ public class GuessNumber {
 
     public String play(int[] answer, int[] inputNumbers) {
         if(is4A08(answer , inputNumbers)) return "4A0B";
-//        if(is0A4B(answer , inputNumbers)) return "0A4B";
-        return "0A0B";
+        if(is0A4B(answer , inputNumbers)) return "0A0B";
+        return null;
     }
 
-//    private boolean is0A4B(int[] answer, int[] inputNumbers) {
-//        for(int i = 0; i < answer.length; i ++ ){
-//            if (answer[i] == inputNumbers[i]) return false;
-//        }
-//        return true;
-//    }
+    private boolean is0A4B(int[] answer, int[] inputNumbers) {
+        for(int i = 0; i < answer.length; i ++ ){
+            if (answer[i] == inputNumbers[i]) return false;
+        }
+        return true;
+    }
 
     private boolean is4A08(int[] answer, int[] inputNumbers) {
         for(int i = 0; i < answer.length; i ++){
