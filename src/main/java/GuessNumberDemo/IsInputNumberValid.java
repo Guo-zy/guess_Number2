414c16e47f34ccacb53985fb19cb6e13b6543ea2
Guo-zy <674402145@qq.com>
2020-07-24 08:45:45 +0800

    [Check Input] [Yancy] feature : pass should_return_input_is_null_when_checkInputNull_given_null

--- src/main/java/GuessNumberDemo/IsInputNumberValid.java
+++ src/main/java/GuessNumberDemo/IsInputNumberValid.java
@@ -3,8 +3,8 @@ package GuessNumberDemo;
 public class IsInputNumberValid {
 
     public String checkInputNull(int[] inputNumbers) {
-        return null;
-//        if(inputNumbers == null || inputNumbers.length == 0)  return "input is null";
-//        return "input is valid";
+
+        if(inputNumbers == null || inputNumbers.length == 0)  return "input is null";
+        return "input is valid";
     }
 }
