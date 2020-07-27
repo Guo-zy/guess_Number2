654fefc145914fcbea53f5e139f7f4dbe8775fd5
Guo-zy <674402145@qq.com>
2020-07-27 21:01:47 +0800

    [Guess Number] [Yancy] refactor : remove all code in GuessNumberProcess and GuessNumberProcessTest

--- src/main/java/GuessNumberDemo/GuessNumberProcess.java
+++ src/main/java/GuessNumberDemo/GuessNumberProcess.java
@@ -3,15 +3,5 @@ package GuessNumberDemo;
 public class GuessNumberProcess {
 
 
-    private String isTryOver6Time(int tryTime) {
-        if(tryTime >= 7) return "tryTime over 6 time , game over";
-        return  null;
-    }
 
-    public String getGameResult(int tryTime){
-        String tryOver6Time = isTryOver6Time(tryTime);
-        if("tryTime over 6 time , game over".equals(tryOver6Time)) return tryOver6Time;
-
-        return null;
-    }
 }
--- src/test/java/GuessNumberDemo/GuessNumberProcessTest.java
+++ src/test/java/GuessNumberDemo/GuessNumberProcessTest.java
@@ -5,21 +5,5 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GuessNumberProcessTest {
-    @Test
-    void should_return_game_over_when_isTry_over_6_time_then_given_tryTime_is_7() {
-        //given
-        GuessNumberProcess guessNumberProcess = new GuessNumberProcess();
-        int tryTime = 7;
 
-        //when
-        String res = guessNumberProcess.getGameResult(tryTime);
-
-        //then
-        assertEquals("tryTime over 6 time , game over" , res );
-    }
-
-    @Test
-    void should_return_wrong_question_when_checkGuessNumber_then_given_wrong_GuessNumber() {
-        //given
-    }
 }
