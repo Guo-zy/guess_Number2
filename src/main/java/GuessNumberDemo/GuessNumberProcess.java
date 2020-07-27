810d5005517609dda8ef82d0b824f22298661860
Guo-zy <674402145@qq.com>
2020-07-27 21:09:46 +0800

    [Guess Number] [Yancy] feature : pass should_return_win_when_getGameResult_given_correctNumber

--- src/main/java/GuessNumberDemo/GuessNumberProcess.java
+++ src/main/java/GuessNumberDemo/GuessNumberProcess.java
@@ -2,6 +2,22 @@ package GuessNumberDemo;
 
 public class GuessNumberProcess {
 
+    private String resultMessage;
 
+    public String getResultMessage() {
+        return resultMessage;
+    }
+
+    public void setResultMessage(String resultMessage) {
+        this.resultMessage = resultMessage;
+    }
+
+    public boolean getGameResult(String guessNumber) {
+            if(guessNumber.equals("4A0B")){
+                setResultMessage("win");
+                return true;
+            }
+            return false;
+    }
 
 }
--- src/test/java/GuessNumberDemo/GuessNumberProcessTest.java
+++ src/test/java/GuessNumberDemo/GuessNumberProcessTest.java
@@ -5,5 +5,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GuessNumberProcessTest {
+    @Test
+    void should_return_win_when_getGameResult_given_correctNumber() {
+        //given
+        GuessNumberProcess guessNumberProcess = new GuessNumberProcess();
+
+        //when
+        guessNumberProcess.getGameResult("4A0B");
+
+        //then
+        assertEquals("win" , guessNumberProcess.getResultMessage());
+
+
+    }
 
 }
